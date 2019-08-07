2d1c11bc71341a76e2b78c3813d1171f70765944
jonamparo <jonathanmamparo@gmail.com>
2019-08-06 22:11:24 -0400

    oop constrcuter

---
+++ javascript-algorithm-and-data-structures-300hrs/object-oriented-programming/5-define-a-constructor-function.js
@@ -0,0 +1,8 @@
+class Dog {
+    constructor() {
+        this.name = "Billy";
+        this.color = "brown";
+        this.numLegs = 4;
+    }
+}
+
---
+++ javascript-algorithm-and-data-structures-300hrs/object-oriented-programming/7-extend-constructors-to-receive-arguments.js
@@ -0,0 +1,9 @@
+class Dog {
+    constructor(name, color) {
+        this.name = name;
+        this.color = color;
+        this.numLegs = 4;
+    }
+}
+
+let terrier = new Dog("blooudhound", "black");
