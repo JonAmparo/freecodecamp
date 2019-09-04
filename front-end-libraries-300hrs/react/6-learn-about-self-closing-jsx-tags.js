720729fb5ef9fdb82f72a95ccfc1c1abce81d622
jonamparo <jonathanmamparo@gmail.com>
2019-09-04 07:19:12 -0400

    react

---
+++ front-end-libraries-300hrs/react/6-learn-about-self-closing-jsx-tags.js
@@ -0,0 +1,8 @@
+const JSX = (
+  <div>
+    {/* remove comment and change code below this line*/}
+    <h2>Welcome to React!</h2> <br />
+    <p>Be sure to close all tags!</p>
+    <hr />
+  </div>
+);
---
+++ front-end-libraries-300hrs/react/7-create-a-stateless-functional-component.js
@@ -0,0 +1,7 @@
+const MyComponent = function() {
+  // change code below this line
+
+  return <div className="customClass">hi</div>;
+
+  // change code above this line
+};
